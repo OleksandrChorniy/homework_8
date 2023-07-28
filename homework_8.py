bb41b52fecd63d55fc22b47f84e23ab67ed6f1a9
OleksandrChorniy <sahacherniy94@gmail.com>
2023-07-28 10:18:32 +0300

    add extension `.py`, and list `users`

--- homework_8.py
+++ homework_8.py
@@ -31,9 +31,16 @@ def get_birthdays_per_week(n):
         if birthdays[current_day.strftime('%A')]:
             print(f"{current_day.strftime('%A')}: {', '.join(birthdays[current_day.strftime('%A')])}")
 
-        
+
+users = [
+    {'name': 'Nastya', 'birthday': datetime(2023, 7, 27)},
+    {'name': 'Yura', 'birthday': datetime(2023, 7, 26)},
+    {'name': 'Sofia', 'birthday': datetime(2023, 7, 25)},
+    {'name': 'Ernest', 'birthday': datetime(2023, 7, 20)},
+]
+
 if __name__ == '__main__':
-    get_birthdays_per_week()
+    (get_birthdays_per_week(users))
 
 
 
